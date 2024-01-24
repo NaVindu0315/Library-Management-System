d07308487de3d1d14ede890e017c7848136bff0a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-25 00:18:10 +0530

    finshed ui added vairables

---
+++ Library Management System/Library Management System/members.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace Library_Management_System
+{
+    partial class members
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtmemid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtadrs = new System.Windows.Forms.TextBox();
+            this.txtdob = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnview = new System.Windows.Forms.Button();
+            this.btnhome = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(310, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Members";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Member ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(136, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(136, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Date of Birth";
+            // 
+            // txtmemid
+            // 
+            this.txtmemid.Location = new System.Drawing.Point(317, 102);
+            this.txtmemid.Name = "txtmemid";
+            this.txtmemid.Size = new System.Drawing.Size(100, 20);
+            this.txtmemid.TabIndex = 5;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(317, 144);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 6;
+            // 
+            // txtadrs
+            // 
+            this.txtadrs.Location = new System.Drawing.Point(317, 192);
+            this.txtadrs.Name = "txtadrs";
+            this.txtadrs.Size = new System.Drawing.Size(100, 20);
+            this.txtadrs.TabIndex = 7;
+            // 
+            // txtdob
+            // 
+            this.txtdob.Location = new System.Drawing.Point(317, 242);
+            this.txtdob.Name = "txtdob";
+            this.txtdob.Size = new System.Drawing.Size(100, 20);
+            this.txtdob.TabIndex = 8;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(466, 308);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 9;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(604, 240);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 10;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // btnview
+            // 
+            this.btnview.Location = new System.Drawing.Point(604, 147);
+            this.btnview.Name = "btnview";
+            this.btnview.Size = new System.Drawing.Size(99, 23);
+            this.btnview.TabIndex = 11;
+            this.btnview.Text = "View Members";
+            this.btnview.UseVisualStyleBackColor = true;
+            // 
+            // btnhome
+            // 
+            this.btnhome.Location = new System.Drawing.Point(12, 22);
+            this.btnhome.Name = "btnhome";
+            this.btnhome.Size = new System.Drawing.Size(75, 23);
+            this.btnhome.TabIndex = 12;
+            this.btnhome.Text = "Home";
+            this.btnhome.UseVisualStyleBackColor = true;
+            this.btnhome.Click += new System.EventHandler(this.btnhome_Click);
+            // 
+            // members
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnhome);
+            this.Controls.Add(this.btnview);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtdob);
+            this.Controls.Add(this.txtadrs);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtmemid);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "members";
+            this.Text = "members";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtmemid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtadrs;
+        private System.Windows.Forms.TextBox txtdob;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnview;
+        private System.Windows.Forms.Button btnhome;
+    }
+}
---
+++ Library Management System/Library Management System/members.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Library_Management_System
+{
+    public partial class members : Form
+    {
+        //db connection
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Databases\lmsdb.mdf;Integrated Security=True;Connect Timeout=30");
+        //variables
+        int memberid;
+        string name, adrs, dob;
+        public members()
+        {
+            InitializeComponent();
+        }
+
+        private void btnhome_Click(object sender, EventArgs e)
+        {
+            Home hm = new Home();
+            hm.Show();
+            this.Hide();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
