e4edd6f14f5cafb8fa35410045bd3a92e1be6a6e
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-24 19:17:18 +0530

    added label

--- Library Management System/Library Management System/Form1.Designer.cs
+++ Library Management System/Library Management System/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Library_Management_System
 {
-    partial class Form1
+    partial class Home
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,37 @@ namespace Library_Management_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Library Management System";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "Home";
+            this.Text = "Library Management System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
