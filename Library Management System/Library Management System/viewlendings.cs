111ae3ee5f4001fcfca58b179d75bacc2a7e4376
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-24 22:10:21 +0530

    added view

---
+++ Library Management System/Library Management System/viewlendings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management_System
+{
+    public partial class viewlendings : Form
+    {
+        public viewlendings()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Home home = new Home();
+            this.Hide();
+            home.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lendings lends = new lendings();
+            lends.Show();
+            this.Hide();
+        }
+    }
+}
