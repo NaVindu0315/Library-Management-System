983ba8f8d7c4d72a3a5b2e6312290d2b4ccfa874
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-24 19:30:53 +0530

    added buttons

--- Library Management System/Library Management System/Form1.Designer.cs
+++ Library Management System/Library Management System/Form1.Designer.cs
@@ -30,6 +30,9 @@ namespace Library_Management_System
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.btnbooks = new System.Windows.Forms.Button();
+            this.btnlending = new System.Windows.Forms.Button();
+            this.btnmembers = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -43,12 +46,42 @@ namespace Library_Management_System
             this.label1.Text = "Library Management System";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // btnbooks
+            // 
+            this.btnbooks.Location = new System.Drawing.Point(211, 171);
+            this.btnbooks.Name = "btnbooks";
+            this.btnbooks.Size = new System.Drawing.Size(75, 23);
+            this.btnbooks.TabIndex = 1;
+            this.btnbooks.Text = "Books";
+            this.btnbooks.UseVisualStyleBackColor = true;
+            // 
+            // btnlending
+            // 
+            this.btnlending.Location = new System.Drawing.Point(372, 171);
+            this.btnlending.Name = "btnlending";
+            this.btnlending.Size = new System.Drawing.Size(75, 23);
+            this.btnlending.TabIndex = 2;
+            this.btnlending.Text = "Lending";
+            this.btnlending.UseVisualStyleBackColor = true;
+            // 
+            // btnmembers
+            // 
+            this.btnmembers.Location = new System.Drawing.Point(519, 171);
+            this.btnmembers.Name = "btnmembers";
+            this.btnmembers.Size = new System.Drawing.Size(75, 23);
+            this.btnmembers.TabIndex = 3;
+            this.btnmembers.Text = "Members";
+            this.btnmembers.UseVisualStyleBackColor = true;
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnmembers);
+            this.Controls.Add(this.btnlending);
+            this.Controls.Add(this.btnbooks);
             this.Controls.Add(this.label1);
             this.Name = "Home";
             this.Text = "Library Management System";
@@ -60,6 +93,9 @@ namespace Library_Management_System
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnbooks;
+        private System.Windows.Forms.Button btnlending;
+        private System.Windows.Forms.Button btnmembers;
     }
 }
 
