34fffbf6b26d5d7df6351619edb6e40d60f96399
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-25 00:57:29 +0530

    added vieew

---
+++ Library Management System/Library Management System/viewmembers.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Library_Management_System
+{
+    partial class viewmembers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnhome = new System.Windows.Forms.Button();
+            this.btnmembers = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Members";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(170, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(439, 210);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnhome
+            // 
+            this.btnhome.Location = new System.Drawing.Point(254, 332);
+            this.btnhome.Name = "btnhome";
+            this.btnhome.Size = new System.Drawing.Size(75, 23);
+            this.btnhome.TabIndex = 2;
+            this.btnhome.Text = "Home";
+            this.btnhome.UseVisualStyleBackColor = true;
+            // 
+            // btnmembers
+            // 
+            this.btnmembers.Location = new System.Drawing.Point(402, 332);
+            this.btnmembers.Name = "btnmembers";
+            this.btnmembers.Size = new System.Drawing.Size(75, 23);
+            this.btnmembers.TabIndex = 3;
+            this.btnmembers.Text = "Members";
+            this.btnmembers.UseVisualStyleBackColor = true;
+            // 
+            // viewmembers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnmembers);
+            this.Controls.Add(this.btnhome);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "viewmembers";
+            this.Text = "viewmembers";
+            this.Load += new System.EventHandler(this.viewmembers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnhome;
+        private System.Windows.Forms.Button btnmembers;
+    }
+}
---
+++ Library Management System/Library Management System/viewmembers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Library_Management_System
+{
+    public partial class viewmembers : Form
+    {
+        //db connection
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Databases\lmsdb.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public viewmembers()
+        {
+            InitializeComponent();
+        }
+
+        private void viewmembers_Load(object sender, EventArgs e)
+        {
+            string qryy = "SELECT * FROM members ";
+            SqlDataAdapter adapter = new SqlDataAdapter(qryy, con);
+            DataSet set = new DataSet();
+            adapter.Fill(set, "members");
+            dataGridView1.DataSource = set.Tables["members"];
+        }
+    }
+}
